a6f06a53de5f1aacfa803af6107f45d8008be876
quynhtrang12 <quynhtrang15122003@gmail.com>
2025-05-16 00:25:56 +0700

    them sua xoa Category

--- database/migrations/2025_05_15_110004_create_categories_table.php
+++ database/migrations/2025_05_15_110004_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name')->unique();
-            $table->text('mo ta')->nullable();
+            $table->text('mo_ta')->nullable();
         });
     }
 
